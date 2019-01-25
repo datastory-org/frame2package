e52b45ba812a04764b1e7a7ad9ed1669ece098a8
Robin Linderborg <robin.linderborg@gmail.com>
2019-01-25 20:38:41 +0100

    Bump version, add setup.cfg

--- setup.py
+++ setup.py
@@ -5,15 +5,16 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="frame2package",
-    version="0.0.1",
+    version="0.0.2",
     author="Robin Linderborg",
     author_email="robin@datastory.org",
     description="Pandas dataframes to DDF packages.",
+    download_url="https://github.com/datastory-org/frame2package/archive/0.0.2.tar.gz",
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
     install_requires=[
-		'pandas',
-        'ddf_utils'
+		'pandas>=0.23',
+        'ddf_utils>=0.5'
      ],
 )
