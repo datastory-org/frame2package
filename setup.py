a818a3020b368f582fe3a47ec3188d110f3afa96
Robin Linderborg <robin.linderborg@gmail.com>
2019-03-12 17:22:26 +0100

    Update ddf_utils version, and package version

--- setup.py
+++ setup.py
@@ -5,16 +5,16 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="frame2package",
-    version="0.0.3",
+    version="0.0.4",
     author="Robin Linderborg",
     author_email="robin@datastory.org",
     description="Pandas dataframes to DDF packages.",
-    download_url="https://github.com/datastory-org/frame2package/archive/0.0.3.tar.gz",
+    download_url="https://github.com/datastory-org/frame2package/archive/0.0.4.tar.gz",
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
     install_requires=[
         'pandas>=0.23',
-        'ddf_utils==0.6.2'
+        'ddf_utils==0.6.4'
      ],
 )
